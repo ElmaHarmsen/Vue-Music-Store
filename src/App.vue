86e66c55437e0de501ae2ffab96e23cc52ee69b8
ElmaHarmsen <elmaharmsen01@gmail.com>
2020-02-12 21:12:29 +0100

    First commit:
    Vue project created. SCSS file created for global style.
    Tried stuff out with a component.
    Only problem is that the downloadable font gets rejected.

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <HelloWorld v-bind:msg="smth" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  data: function() {
+    return {
+      //always a function with a return
+      smth: 0
+    };
+  },
+  components: {
+    HelloWorld
+  },
+  created: function() {
+    //always a function
+    setInterval(() => {
+      this.smth = Math.random();
+    }, 1000);
+  },
+  watch: {
+    smth: function(theNewValue, theValueItWasBefore) {
+      console.log(
+        `The old value was: ${theValueItWasBefore}, the new one is ${theNewValue}`
+      );
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h3>
+      {{ msg }}
+    </h3>
+    <article>
+      <h3>Formidable Music Store</h3>
+      <p>This is my first Vue project</p>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: Number
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+article {
+  h3 {
+    font-family: "dancing-script";
+    color: $merlit-purple-color;
+  }
+  p {
+    font-family: "segou-ui";
+    color: $throbbing-dark-color;
+  }
+}
+</style>
